94266e1bfe871555d4574a23b660dc1015732a5c
Loren Sands-Ramshaw <lorensr@gmail.com>
2017-10-27 04:07:18 -0400

    Add src/components/ dir

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import App from './App'
+import App from './components/App'
 import registerServiceWorker from './registerServiceWorker'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
 registerServiceWorker()
+
+module.hot.accept()
