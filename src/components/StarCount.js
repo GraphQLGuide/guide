1705889314903cf13b725412fa4d3e7ef086f040
Loren Sands-Ramshaw <lorensr@gmail.com>
2017-11-29 19:10:35 -0500

    Add STARS_SUBSCRIPTION

--- src/components/StarCount.js
+++ src/components/StarCount.js
@@ -5,20 +5,44 @@ import gql from 'graphql-tag'
 import classNames from 'classnames'
 import Odometer from 'react-odometerjs'
 
-const StarCount = ({ githubStars, loading }) => {
-  return (
-    <a
-      className={classNames('StarCount', { loading })}
-      href="https://github.com/GraphQLGuide/guide"
-    >
-      {githubStars && <Odometer value={githubStars} />}
-    </a>
-  )
+class StarCount extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  componentDidMount() {
+    this.props.subscribeToMore({
+      document: STARS_SUBSCRIPTION,
+      updateQuery: (
+        previousResult,
+        {
+          subscriptionData: {
+            data: { githubStars }
+          }
+        }
+      ) => ({ githubStars })
+    })
+  }
+
+  render() {
+    const { githubStars, loading } = this.props
+
+    return (
+      <a
+        className={classNames('StarCount', { loading })}
+        href="https://github.com/GraphQLGuide/guide"
+      >
+        {githubStars && <Odometer value={githubStars} />}
+      </a>
+    )
+  }
 }
 
 StarCount.propTypes = {
   githubStars: PropTypes.number,
-  loading: PropTypes.bool.isRequired
+  loading: PropTypes.bool.isRequired,
+  subscribeToMore: PropTypes.func.isRequired
 }
 
 const STARS_QUERY = gql`
@@ -27,10 +51,20 @@ const STARS_QUERY = gql`
   }
 `
 
+const STARS_SUBSCRIPTION = gql`
+  subscription StarsSubscription {
+    githubStars
+  }
+`
+
 export default () => (
-  <Query query={STARS_QUERY} pollInterval={5 * 1000}>
-    {({ data: { githubStars }, loading }) => (
-      <StarCount githubStars={githubStars} loading={loading} />
+  <Query query={STARS_QUERY}>
+    {({ data: { githubStars }, loading, subscribeToMore }) => (
+      <StarCount
+        githubStars={githubStars}
+        loading={loading}
+        subscribeToMore={subscribeToMore}
+      />
     )}
   </Query>
 )
