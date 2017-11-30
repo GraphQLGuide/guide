c5b76e6131a2d81695cf3af262943fc7c541f974
Loren Sands-Ramshaw <lorensr@gmail.com>
2017-11-29 19:10:35 -0500

    Add STARS_SUBSCRIPTION

--- src/index.js
+++ src/index.js
@@ -6,11 +6,30 @@ import registerServiceWorker from './registerServiceWorker'
 import { ApolloClient } from 'apollo-client'
 import { ApolloProvider } from 'react-apollo'
 import { InMemoryCache } from 'apollo-cache-inmemory'
+import { split } from 'apollo-link'
+import { WebSocketLink } from 'apollo-link-ws'
 import { createHttpLink } from 'apollo-link-http'
+import { getMainDefinition } from 'apollo-utilities'
 
-const link = createHttpLink({
+const httpLink = createHttpLink({
   uri: 'https://api.graphql.guide/graphql'
 })
+
+const wsLink = new WebSocketLink({
+  uri: `wss://api.graphql.guide/websocket`,
+  options: {
+    reconnect: true
+  }
+})
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query)
+    return kind === 'OperationDefinition' && operation === 'subscription'
+  },
+  wsLink,
+  httpLink
+)
 
 const cache = new InMemoryCache()
 
