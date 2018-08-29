89cf4c2bd7a81e6fe19aeaa0e120cde5b98a90af
Loren 🤓 <lorensr@gmail.com>
2018-08-28 22:19:46 -0400

    withAuth -> withUser, fixes #33

---
+++ src/lib/apollo.js
@@ -0,0 +1,51 @@
+import { ApolloClient } from 'apollo-client'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { split } from 'apollo-link'
+import { WebSocketLink } from 'apollo-link-ws'
+import { createHttpLink } from 'apollo-link-http'
+import { getMainDefinition } from 'apollo-utilities'
+import { setContext } from 'apollo-link-context'
+import { getAuthToken } from 'auth0-helpers'
+
+const httpLink = createHttpLink({
+  uri: 'https://api.graphql.guide/graphql'
+})
+
+const authLink = setContext(async (_, { headers }) => {
+  const token = await getAuthToken({
+    doLoginIfTokenExpired: true
+  })
+
+  if (token) {
+    return {
+      headers: {
+        ...headers,
+        authorization: `Bearer ${token}`
+      }
+    }
+  } else {
+    return { headers }
+  }
+})
+
+const authedHttpLink = authLink.concat(httpLink)
+
+const wsLink = new WebSocketLink({
+  uri: `wss://api.graphql.guide/subscriptions`,
+  options: {
+    reconnect: true
+  }
+})
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query)
+    return kind === 'OperationDefinition' && operation === 'subscription'
+  },
+  wsLink,
+  authedHttpLink
+)
+
+const cache = new InMemoryCache()
+
+export const apollo = new ApolloClient({ link, cache })
