95fc58274b60f6d66c66698f444e352e0099a993
Loren 🤓 <lorensr@gmail.com>
2018-08-30 00:24:19 -0400

    componentWillReceiveProps -> componentDidUpdate

--- src/index.js
+++ src/index.js
@@ -2,25 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { ApolloProvider } from 'react-apollo'
 import { BrowserRouter } from 'react-router-dom'
-import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles'
 
 import './index.css'
 import registerServiceWorker from './registerServiceWorker'
 import App from './components/App'
 import { apollo } from './lib/apollo'
 
-const GRAPHQL_PINK = '#e10098'
-
-const theme = createMuiTheme({
-  palette: { primary: { main: GRAPHQL_PINK } }
-})
-
 ReactDOM.render(
   <BrowserRouter>
     <ApolloProvider client={apollo}>
-      <MuiThemeProvider theme={theme}>
-        <App />
-      </MuiThemeProvider>
+      <App />
     </ApolloProvider>
   </BrowserRouter>,
   document.getElementById('root')
