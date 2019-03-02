dd13c9b5b4780bb17f8ebdd7601bbcc71a3bce04
Loren 🤓 <lorensr@gmail.com>
2019-03-01 22:42:23 -0500

    Update to Apollo 2.5

--- src/lib/apollo.js
+++ src/lib/apollo.js
@@ -6,8 +6,8 @@ import { createHttpLink } from 'apollo-link-http'
 import { getMainDefinition } from 'apollo-utilities'
 import { setContext } from 'apollo-link-context'
 import { getAuthToken } from 'auth0-helpers'
-import { withClientState } from 'apollo-link-state'
 import { RestLink } from 'apollo-link-rest'
+import gql from 'graphql-tag'
 
 import { errorLink } from './errorLink'
 
@@ -50,6 +50,12 @@ const networkLink = split(
   authedHttpLink
 )
 
+const restLink = new RestLink({
+  uri: 'https://api.openweathermap.org/data/2.5/'
+})
+
+const link = ApolloLink.from([errorLink, restLink, networkLink])
+
 export const cache = new InMemoryCache({
   cacheRedirects: {
     Query: {
@@ -59,19 +65,19 @@ export const cache = new InMemoryCache({
   }
 })
 
-const stateLink = withClientState({
+const typeDefs = gql`
+  type Query {
+    loginInProgress: Boolean
+  }
+  type Mutation {
+    setSectionScroll(id: String!, scrollY: Int!): Boolean
+  }
+`
+
+export const apollo = new ApolloClient({
+  link,
   cache,
-  defaults: {
-    loginInProgress: false
-  },
-  typeDefs: `
-    type Query {
-      loginInProgress: Boolean
-    }
-    type Mutation {
-      setSectionScroll(id: String!, scrollY: Int!): Boolean
-    }
-  `,
+  typeDefs,
   resolvers: {
     Section: {
       scrollY: () => 0
@@ -86,13 +92,17 @@ const stateLink = withClientState({
   }
 })
 
-const restLink = new RestLink({
-  uri: 'https://api.openweathermap.org/data/2.5/'
-})
+const initializeCache = () => {
+  cache.writeData({
+    data: {
+      loginInProgress: false
+    }
+  })
+}
 
-const link = ApolloLink.from([errorLink, stateLink, restLink, networkLink])
+initializeCache()
 
-export const apollo = new ApolloClient({ link, cache })
+apollo.onResetStore(initializeCache)
 
 export const apolloSpace = new ApolloClient({
   link: ApolloLink.from([
