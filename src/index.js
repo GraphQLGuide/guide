7d1e6ed0a94a126f9ce2cf5ccdab26cefa83fd6e
Loren 🤓 <lorensr@gmail.com>
2020-08-11 01:39:22 -0400

    Update CRA and packages

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { render } from 'react-dom'
 import './index.css'
 import App from './components/App'
-import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
-
-registerServiceWorker()
+render(<App />, document.getElementById('root'))
 
 module.hot.accept()
