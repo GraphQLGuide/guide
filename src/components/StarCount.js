7224266f34e07c5a25ac9ec6a0bee6335c211fbb
Loren 🤓 <lorensr@gmail.com>
2020-08-19 01:55:37 -0400

    Update CRA and packages

--- src/components/StarCount.js
+++ src/components/StarCount.js
@@ -1,27 +1,7 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import { Query } from 'react-apollo'
-import gql from 'graphql-tag'
+import { gql, useQuery } from '@apollo/client'
 import classNames from 'classnames'
 import Odometer from 'react-odometerjs'
-
-const StarCount = ({ githubStars, loading }) => {
-  return (
-    <a
-      className={classNames('StarCount', { loading })}
-      href="https://github.com/GraphQLGuide/guide"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      {githubStars && <Odometer value={githubStars} />}
-    </a>
-  )
-}
-
-StarCount.propTypes = {
-  githubStars: PropTypes.number,
-  loading: PropTypes.bool.isRequired
-}
 
 const STARS_QUERY = gql`
   query StarsQuery {
@@ -29,10 +9,19 @@ const STARS_QUERY = gql`
   }
 `
 
-export default () => (
-  <Query query={STARS_QUERY} pollInterval={5 * 1000}>
-    {({ data: { githubStars }, loading }) => (
-      <StarCount githubStars={githubStars} loading={loading} />
-    )}
-  </Query>
-)
+export default () => {
+  const { data, loading } = useQuery(STARS_QUERY, {
+    pollInterval: 5 * 1000,
+  })
+
+  return (
+    <a
+      className={classNames('StarCount', { loading })}
+      href="https://github.com/GraphQLGuide/guide"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      {data && <Odometer value={data.githubStars} />}
+    </a>
+  )
+}
