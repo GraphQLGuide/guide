4e4ff589a4048707a9da09ad579bf81eeca55d54
Loren 🤓 <lorensr@gmail.com>
2020-08-20 03:44:26 -0400

    Update CRA and packages

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,43 +1,8 @@
 import React from 'react'
 import Skeleton from 'react-loading-skeleton'
-import PropTypes from 'prop-types'
-import { graphql } from 'react-apollo'
-import gql from 'graphql-tag'
-import { withRouter } from 'react-router'
-
-const Section = ({
-  loading,
-  sectionContent,
-  location: { state: { chapter, section } }
-}) => (
-  <section className="Section">
-    <div className="Section-header-wrapper">
-      <header className="Section-header">
-        {chapter.number !== null ? (
-          <div>
-            <h1>{section.title}</h1>
-            <h2>
-              {'Chapter ' + chapter.number}
-              <span className="Section-number-divider" />
-              {'Section ' + section.number}
-            </h2>
-          </div>
-        ) : (
-          <h1>{chapter.title}</h1>
-        )}
-      </header>
-    </div>
-    <div className="Section-content">
-      {loading ? <Skeleton count={7} /> : sectionContent}
-    </div>
-  </section>
-)
-
-Section.propTypes = {
-  sectionContent: PropTypes.string,
-  location: PropTypes.object.isRequired,
-  loading: PropTypes.bool.isRequired
-}
+import { gql, useQuery } from '@apollo/client'
+import { useLocation } from 'react-router'
+import get from 'lodash/get'
 
 const SECTION_QUERY = gql`
   query SectionContent($id: String!) {
@@ -47,14 +12,36 @@ const SECTION_QUERY = gql`
   }
 `
 
-const withData = graphql(SECTION_QUERY, {
-  options: ({ location: { state: { section: { id } } } }) => ({
-    variables: { id }
-  }),
-  props: ({ data: { section, loading } }) => ({
-    sectionContent: section && section.content,
-    loading
-  })
-})
+export default () => {
+  const {
+    state: { section, chapter },
+  } = useLocation()
 
-export default withRouter(withData(Section))
+  const { data, loading } = useQuery(SECTION_QUERY, {
+    variables: { id: section.id },
+  })
+
+  return (
+    <section className="Section">
+      <div className="Section-header-wrapper">
+        <header className="Section-header">
+          {chapter.number !== null ? (
+            <div>
+              <h1>{section.title}</h1>
+              <h2>
+                {'Chapter ' + chapter.number}
+                <span className="Section-number-divider" />
+                {'Section ' + section.number}
+              </h2>
+            </div>
+          ) : (
+            <h1>{chapter.title}</h1>
+          )}
+        </header>
+      </div>
+      <div className="Section-content">
+        {loading ? <Skeleton count={7} /> : get(data, 'section.content')}
+      </div>
+    </section>
+  )
+}
--- src/components/TableOfContents.js
+++ src/components/TableOfContents.js
@@ -1,7 +1,5 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import { graphql } from 'react-apollo'
-import gql from 'graphql-tag'
+import { gql, useQuery } from '@apollo/client'
 import Skeleton from 'react-loading-skeleton'
 import { NavLink } from 'react-router-dom'
 import classNames from 'classnames'
@@ -16,82 +14,6 @@ const LoadingSkeleton = () => (
     <Skeleton count={4} />
   </div>
 )
-
-const TableOfContents = ({ chapters, loading }) => (
-  <nav className="TableOfContents">
-    {loading ? (
-      <LoadingSkeleton />
-    ) : (
-      <ul className="TableOfContents-chapters">
-        {chapters.map(chapter => {
-          const chapterIsNumbered = chapter.number !== null
-          return (
-            <li
-              className={classNames({ numbered: chapterIsNumbered })}
-              key={chapter.id}
-            >
-              <NavLink
-                to={{
-                  pathname: slugify(chapter),
-                  state: { chapter, section: chapter.sections[0] }
-                }}
-                className="TableOfContents-chapter-link"
-                activeClassName="active"
-                isActive={(match, location) => {
-                  const rootPath = location.pathname.split('/')[1]
-                  return rootPath.includes(withHyphens(chapter.title))
-                }}
-              >
-                {chapterIsNumbered && (
-                  <span className="TableOfContents-chapter-number">
-                    {chapter.number}
-                  </span>
-                )}
-                {chapter.title}
-              </NavLink>
-              {chapterIsNumbered && (
-                <ul className="TableOfContents-sections">
-                  {chapter.sections.map(section => (
-                    <li key={section.number}>
-                      <NavLink
-                        to={{
-                          pathname: slugify(chapter, section),
-                          state: { chapter, section }
-                        }}
-                        className="TableOfContents-section-link"
-                        activeClassName="active"
-                      >
-                        {section.title}
-                      </NavLink>
-                    </li>
-                  ))}
-                </ul>
-              )}
-            </li>
-          )
-        })}
-      </ul>
-    )}
-  </nav>
-)
-
-TableOfContents.propTypes = {
-  chapters: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      number: PropTypes.number,
-      title: PropTypes.string.isRequired,
-      sections: PropTypes.arrayOf(
-        PropTypes.shape({
-          id: PropTypes.string.isRequired,
-          number: PropTypes.number.isRequired,
-          title: PropTypes.string
-        }).isRequired
-      ).isRequired
-    }).isRequired
-  ),
-  loading: PropTypes.bool.isRequired
-}
 
 const CHAPTER_QUERY = gql`
   query ChapterQuery {
@@ -108,11 +30,64 @@ const CHAPTER_QUERY = gql`
   }
 `
 
-const withData = graphql(CHAPTER_QUERY, {
-  props: ({ data: { chapters, loading } }) => ({
-    chapters,
-    loading
-  })
-})
+export default () => {
+  const { data: { chapters } = {}, loading } = useQuery(CHAPTER_QUERY)
 
-export default withData(TableOfContents)
+  return (
+    <nav className="TableOfContents">
+      {loading ? (
+        <LoadingSkeleton />
+      ) : (
+        <ul className="TableOfContents-chapters">
+          {chapters.map((chapter) => {
+            const chapterIsNumbered = chapter.number !== null
+            return (
+              <li
+                className={classNames({ numbered: chapterIsNumbered })}
+                key={chapter.id}
+              >
+                <NavLink
+                  to={{
+                    pathname: slugify(chapter),
+                    state: { chapter, section: chapter.sections[0] },
+                  }}
+                  className="TableOfContents-chapter-link"
+                  activeClassName="active"
+                  isActive={(_, location) => {
+                    const rootPath = location.pathname.split('/')[1]
+                    return rootPath.includes(withHyphens(chapter.title))
+                  }}
+                >
+                  {chapterIsNumbered && (
+                    <span className="TableOfContents-chapter-number">
+                      {chapter.number}
+                    </span>
+                  )}
+                  {chapter.title}
+                </NavLink>
+                {chapterIsNumbered && (
+                  <ul className="TableOfContents-sections">
+                    {chapter.sections.map((section) => (
+                      <li key={section.number}>
+                        <NavLink
+                          to={{
+                            pathname: slugify(chapter, section),
+                            state: { chapter, section },
+                          }}
+                          className="TableOfContents-section-link"
+                          activeClassName="active"
+                        >
+                          {section.title}
+                        </NavLink>
+                      </li>
+                    ))}
+                  </ul>
+                )}
+              </li>
+            )
+          })}
+        </ul>
+      )}
+    </nav>
+  )
+}
