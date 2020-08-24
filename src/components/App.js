ec7103972a8664a2e2adf7cc8601299f0ebdfefc
Loren 🤓 <lorensr@gmail.com>
2020-08-24 01:41:52 -0400

    Make <App> a function

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,17 @@
-import React, { Component } from 'react'
+import React from 'react'
+
 import logo from '../logo.svg'
 import StarCount from './StarCount'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <StarCount />
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">The GraphQL Guide</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code>, and save to reload.
-        </p>
-      </div>
-    )
-  }
-}
-
-export default App
+export default () => (
+  <div className="App">
+    <header className="App-header">
+      <StarCount />
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="App-title">The GraphQL Guide</h1>
+    </header>
+    <p className="App-intro">
+      To get started, edit <code>src/App.js</code>, and save to reload.
+    </p>
+  </div>
+)
